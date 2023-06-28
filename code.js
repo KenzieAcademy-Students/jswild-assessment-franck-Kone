9b661533b2f0a12e16d3784cc2cbee0391beb0c0
franck <franck.kone@mykenzie.snhu.edu>
2023-06-27 21:06:01 -0400

    complete jswild API week 21

--- code.js
+++ code.js
@@ -1,27 +1,33 @@
 // To run this assignment, right click on index.html in the Visual Studio file explorer to the left
 // and select "Open with Live Server"
 
+// Your Code Here.
+
 //Clean Body
 document.body.textContent = ''
 
 // create header
 const header = `<header id="header">
 <h1 id="title"><span>&#128506;</span>JavaScript in The Wild LOCATOR APP </h1>
-<button id="find-me" class="find-me">Where Am I</button>
+<button id="find-me" class="find-me">Locator</button>
 </header>`
 
 // create coords container
 const coordsContainer = `<div class ='coords'>
-
-<a id="map-link" target="_blank"></a>
+<a id="map-link" target="_blank"></a><i class="fa-solid fa-eye"></i>
 </div>`
-// < p id = "status" ></>
-//Append header to the body
+
+//Instruction message
+const instruction = document.createElement('h1')
+instruction.className = 'instruction'
+instruction.innerHTML = "Click On The Locator Button Up There <span>👆🏿</span> To See Your Current Position"
+
+//Append header,coordsContainer and instruction message to the body
 document.body.innerHTML = header
 document.body.innerHTML += coordsContainer
+document.body.append(instruction)
 
-
-// Your Code Here.
+//Get coords latitude and longitude with google geolocation api
 let latitudeCoord, longitudeCoord
 
 function success(position) {
@@ -30,186 +36,166 @@ function success(position) {
     latitudeCoord = coords.latitude
     longitudeCoord = coords.longitude
 
-    const page1 = getData(latitudeCoord, longitudeCoord)
-    // console.log(page1)
- 
-     
+    getData(latitudeCoord, longitudeCoord)
 }
-
 
 function error() {
     alert("Sorry, no position available.");
 }
 
 const options = {
-    enableHighAccuracy: true,
+    enableHighAccuracy: false,
     maximumAge: 0,
     timeout: 5000,
 };
 
-
 navigator.geolocation.getCurrentPosition(success, error, options)
 
-
-
+// Display coords and photos to the screen when pressing the button named Locator 
 document.querySelector("#find-me").addEventListener("click", () => {
-    const mapLink = document.querySelector("#map-link");
-    mapLink.textContent = `Latitude: ${latitudeCoord} °, Longitude: ${longitudeCoord} °`;
 
+    const coordsDisplay = document.querySelector("#map-link");
+    coordsDisplay.textContent = `Latitude: ${latitudeCoord} °, Longitude: ${longitudeCoord} °`;
+
+    document.querySelector('.images-stock').style.display = 'flex'
+    instruction.style.display = 'none'
+
+    document.querySelector('.coords').style.visibility = 'visible'
 });
 
+// Hide coords Display when clicking on eye fa-solid font awesome
+let coordsIsHidden = false
+const eye = document.querySelector('.fa-solid') 
+eye.onclick = function (){
+    
+    if(!coordsIsHidden){
+        document.querySelector('#map-link').style.visibility = 'hidden'
+        eye.classList.remove('fa-eye')
+        eye.classList.add('fa-eye-slash')
+        eye.style.color = 'black'
+        eye.style.animation = 'none'
 
+        coordsIsHidden = true
+    }else{
+        document.querySelector('#map-link').style.visibility = 'visible'
+        eye.classList.remove('fa-eye-slash')
+        eye.classList.add('fa-eye')
+        eye.style.color = 'brown'
+        eye.style.animation = 'textFlash 1s ease-in-out infinite'
+
+        coordsIsHidden = false
+    }
+   
+}
+
+//Get data from flickr api based on our geolocation 
 const getData = async function (lat, lon) {
 
     const apiKey = "b2cfde4baf7b243140c2a6e2d2c9940c";
 
-    const myResponse = await fetch(`https://www.flickr.com/services/rest/?api_key=${apiKey}&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&lat=${lat}&lon=${lon}`)
-
+    const myResponse = await fetch(`https://www.flickr.com/services/rest/?api_key=${apiKey}&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&lat=${lat}&lon=${lon}&text=bridge&page=2`)
         .then((response) => {
 
             if (response.status !== 200) {
                 throw new Error('Cannot Either Fecth Data or Get Your Coords')
             }
             const newResponse = response.json()
-     
             return newResponse
         })
-
         .then((result) => {
-        
+
             renderImages(result)
         })
 
     return myResponse
 }
 
-function imageUrl(page, num) {
- 
-    return `https://live.staticflickr.com/${page.photos.photo[num].server}/${page.photos.photo[num].id}_${page.photos.photo[num].secret}_c.jpg`
+//build image url
+function createImageUrl(apiResult, num) {
+
+    return `https://live.staticflickr.com/${apiResult.photos.photo[num].server}/${apiResult.photos.photo[num].id}_${apiResult.photos.photo[num].secret}_c.jpg`
 }
 
-function renderImages(promise){
-    
-    const imagesStock = document.createElement('section')
-    imagesStock.className = "images-stock"
-    const imagesDivSlide = document.createElement('div')
-    imagesDivSlide.setAttribute('class', 'imagesContainer')
+//create Html image tags in according to our api Response and div and set their source to be images url  that we have created by using createImageUrl function 
+function renderImages(dataFetched) {
 
-    for(let i = 0; i < promise.photos.photo.length; i++){
-        const image = imageUrl(promise, i)
-        const imgHtmlTag = document.createElement('img')
-        imgHtmlTag.setAttribute('style', 'width: 700px; height: 350px')
-        imgHtmlTag.style.minWidth = '700px'
-        imgHtmlTag.src = image
-        imagesDivSlide.append(imgHtmlTag)
+    const imagesBigStock = document.createElement('section')
+    imagesBigStock.className = "images-stock"
+    const allImagesContainer = document.createElement('div')
+    allImagesContainer.setAttribute('class', 'imagesContainer')
 
+    for (let i = 0; i < dataFetched.photos.photo.length; i++) {
+        const newImageUrl = createImageUrl(dataFetched, i)
+        const newImage = document.createElement('img')
+        newImage.setAttribute('style', 'width: 100%; height: 100%')
+        newImage.src = newImageUrl
+        allImagesContainer.append(newImage)
     }
-    imagesStock.append(imagesDivSlide)
-    document.body.append(imagesStock)
-    updateImage()
 
+    imagesBigStock.append(allImagesContainer)
+    document.body.append(imagesBigStock)
+
+    updateImage()
+    createButtonsControl()
 }
+
+// update current image (carousel)
 let currentImage = 1
 let myInterval;
-function updateImage (){
 
-    const allImages = document.querySelectorAll('img')
-    if (currentImage > allImages.length ) {
+function updateImage() {
+
+    const getImagesNodes = document.querySelectorAll('img')
+    if (currentImage > getImagesNodes.length) {
         currentImage = 1
-    }else if(currentImage < 1){
-        currentImage = allImages.length
+    } else if (currentImage < 1) {
+        currentImage = getImagesNodes.length
     }
-    document.querySelector('.imagesContainer').style.transform = `translateX(-${(currentImage -1) * 700}px)`
+    const allImagesContainer = document.querySelector('.imagesContainer')
+    allImagesContainer.style.transform = `translateX(-${(currentImage - 1) * allImagesContainer.clientWidth}px)`
 
-    if(!myInterval){
+    if (!myInterval) {
 
-        myInterval = setInterval(()=> {
+        myInterval = setInterval(() => {
             currentImage++
-             updateImage()
-        },3000)
+            updateImage()
+        }, 3000)
     }
-    // setInterval(()=> {
-    //     currentImage++
-    //     updateImage()
-    // },3000)
 }
 
+// carousel  control buttons creation
+function createButtonsControl() {
 
-// https://live.staticflickr.com/{server-id}/{id}_{o-secret}_o.{o-format}
+    const buttonLeft = document.createElement('button')
+    buttonLeft.className = "btnLeft"
+    buttonLeft.textContent = '«'
 
-// #
-// # Example
-// #   server - id: 7372
-// #  photo - id: 12502775644
-// #   secret: acfd415fa7
-// #   size: w
-// #
+    const buttonRight = document.createElement('button')
+    buttonRight.className = "btnRight"
+    buttonRight.textContent = '»'
 
-// https://live.staticflickr.com/7372/12502775644_acfd415fa7_w.jpg
+    document.querySelector('.images-stock').append(buttonLeft, buttonRight)
 
-// {
-//     "photos": {
-//         "page": 1,
-//             "pages": 4515,
-//                 "perpage": 5,
-//                     "total": 22571,
-//                         "photo": [
-//                             {
-//                                 "id": "53001052908",
-//                                 "owner": "95302179@N08",
-//                                 "secret": "942ecebcba",
-//                                 "server": "65535",
-//                                 "farm": 66,
-//                                 "title": "41",
-//                                 "ispublic": 1,
-//                                 "isfriend": 0,
-//                                 "isfamily": 0
-//                             },
-//                             {
-//                                 "id": "52999978502",
-//                                 "owner": "95302179@N08",
-//                                 "secret": "de47c68505",
-//                                 "server": "65535",
-//                                 "farm": 66,
-//                                 "title": "40",
-//                                 "ispublic": 1,
-//                                 "isfriend": 0,
-//                                 "isfamily": 0
-//                             },
-//                             {
-//                                 "id": "52976975093",
-//                                 "owner": "33712242@N00",
-//                                 "secret": "df97efa559",
-//                                 "server": "65535",
-//                                 "farm": 66,
-//                                 "title": "Former Gimbels Langhorne",
-//                                 "ispublic": 1,
-//                                 "isfriend": 0,
-//                                 "isfamily": 0
-//                             },
-//                             {
-//                                 "id": "52950302625",
-//                                 "owner": "95302179@N08",
-//                                 "secret": "7d9b048139",
-//                                 "server": "65535",
-//                                 "farm": 66,
-//                                 "title": "dji_fly_20230604_081438_448_1685881120618_photo",
-//                                 "ispublic": 1,
-//                                 "isfriend": 0,
-//                                 "isfamily": 0
-//                             },
-//                             {
-//                                 "id": "52950302605",
-//                                 "owner": "95302179@N08",
-//                                 "secret": "a6982a2066",
-//                                 "server": "65535",
-//                                 "farm": 66,
-//                                 "title": "dji_fly_20230604_081644_451_1685881131450_photo",
-//                                 "ispublic": 1,
-//                                 "isfriend": 0,
-//                                 "isfamily": 0
-//                             }
-//                         ]
-//     },
-//     "stat": "ok"
-// }
+    functionalities(buttonLeft, buttonRight)
+
+}
+
+// control buttons functionalities
+function functionalities(left, right) {
+
+    left.addEventListener('click', () => {
+        left.style.transform = 'scale(0.9)'
+        clearInterval(myInterval)
+        myInterval = null
+        currentImage--
+        updateImage()
+    })
+    right.addEventListener('click', () => {
+        right.style.transform = 'scale(0.9)'
+        clearInterval(myInterval)
+        myInterval = null
+        currentImage++
+        updateImage()
+    })
+}
+
